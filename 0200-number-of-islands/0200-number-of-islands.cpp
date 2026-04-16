d2e8e8041127466aa75e17c7ab6909d105cd871b
Tanish <142775471+Tanishsnghhh@users.noreply.github.com>
2026-04-16 20:32:32 +0530

    Time: 26 ms (63.36%), Space: 16.1 MB (88.81%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& arr , int i , int j){
-        if(i< 0 || j <0 ||i>=arr.size() || j>=arr[0].size() || arr[i][j]=='0') {
-            return; 
-            }
-        arr[i][j]='0';
-        dfs(arr,i,j-1);
-        dfs(arr,i,j+1);
-        dfs(arr,i-1,j);
-        dfs(arr,i+1,j); 
+    void dfs(vector<vector<char>>& grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() ||
+            grid[i][j] == '0') {
+            return;
+        }
+        grid[i][j] = '0';
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
     }
-    int numIslands(vector<vector<char>>& arr) {
-        int count =0,n=arr.size(),m=arr[0].size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(arr[i][j]=='1'){
+
+    int numIslands(vector<vector<char>>& grid) {
+        int count = 0;
+        int n = grid.size();
+        int m = grid[0].size();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == '1') {
                     count++;
-                    dfs(arr ,i ,j );
+                    dfs(grid, i, j);
                 }
             }
         }
